73cdaff36d03a4fcd58cd791d3bc1ac37f3c2772
Shad <shad.aeons@gmail.com>
2026-06-23 01:22:53 +0200

    chore: add toWorldPosition function

--- src/renderer/Camera.ts
+++ src/renderer/Camera.ts
@@ -1,3 +1,4 @@
+import { MAX_ZOOM, MIN_ZOOM } from '../constants';
 import type { MapBounds, Vector } from '../types';
 
 export default class Camera {
@@ -12,22 +13,25 @@ export default class Camera {
         return this._zoom;
     }
 
+    public toWorldPosition(screenPos: Vector): Vector {
+        return {
+            x: screenPos.x / this._zoom - this._position.x,
+            y: screenPos.y / this._zoom - this._position.y,
+        };
+    }
+
     public pan(d: Vector) {
         this._position.x += d.x;
         this._position.y += d.y;
     }
 
     public zoomTo(factor: number, c: Vector) {
-        const worldX = c.x / this._zoom - this._position.x;
-        const worldY = c.y / this._zoom - this._position.y;
-
-        const zoom = Math.max(0.1, Math.min(this._zoom * factor, 40));
-
-        this._position = {
-            x: c.x / zoom - worldX,
-            y: c.y / zoom - worldY,
-        };
-        this._zoom = zoom;
+        this._position = this.toWorldPosition(c);
+        this._zoom = Math.max(
+            MIN_ZOOM,
+            Math.min(this._zoom * factor, MAX_ZOOM)
+        );
+        this._position = this.toWorldPosition(c);
     }
 
     public fit(
